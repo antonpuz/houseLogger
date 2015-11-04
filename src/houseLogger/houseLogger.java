4964d3f2ff70e381d86475e62a7334ce6a43f5fb
AntonP <poznov@post.bgu.ac.il>
2015-11-04 21:35:41 +0200

    commit first code

---
+++ src/houseLogger/houseLogger.java
@@ -0,0 +1,12 @@
+package houseLogger;
+
+public class houseLogger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello my kind friends");
+		
+	}
+
+}
