47b30f5776d39727464bd56069bee13d55476a7f
AntonP <poznov@post.bgu.ac.il>
2015-11-04 21:53:35 +0200

    adding logger capabilities

--- src/houseLogger/houseLogger.java
+++ src/houseLogger/houseLogger.java
@@ -1,11 +1,18 @@
 package houseLogger;
 
-public class houseLogger {
+import org.apache.log4j.Logger;
 
+public class houseLogger {
+	
+	private static Logger logger=Logger.getLogger("LoggingExample");
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello my kind friends");
+		
+		logger.error("######    Started a new run    #######");
+		
 		
 	}
 
